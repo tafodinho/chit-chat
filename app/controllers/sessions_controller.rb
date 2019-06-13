28c37def744391c42c63f89f69ca9837039dd770
Tafang Joshua <tafodinho@gmail.com>
2019-06-13 11:46:36 +0100

    Finished implementing login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       flash[:success] = "Successful Login"
+      log_in user
+      redirect_to user
     else 
       flash.now[:danger] = "Invalid login"
       render 'new'
     end
+  end
+
+  def destroy 
+    log_out
+    redirect_to root_url
   end
 end
