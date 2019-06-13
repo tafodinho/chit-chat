8722e235715a386d324fd863d18208b3ced723c0
offorma <offorma@gmail.com>
2019-06-13 17:47:18 +0100

    more tests and validation for microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,4 +48,11 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
