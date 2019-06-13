3cf35cfa42666874d2aab050699545dffd83f595
offorma <offorma@gmail.com>
2019-06-13 22:17:40 +0100

    can add new post now

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,5 +3,13 @@ class MicropostsController < ApplicationController
     def new
     end
     def create
+        @micropost = @current_user.microposts.create(micropost_params)
+        flash[:success] = "New post created"
+        redirect_to new_micropost_path
+    end
+     
+    private
+    def micropost_params
+      params.require(:micropost).permit(:content)
     end
 end
