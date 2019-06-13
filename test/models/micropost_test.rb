094f4fce93550f87ec921a6b720de7519a331b16
offorma <offorma@gmail.com>
2019-06-13 17:16:23 +0100

    wrote test for micropost model

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
