3afdbfb0df20439ea750fbc820fe087c15ffc281
Tafang Joshua <tafodinho@gmail.com>
2019-09-30 22:50:19 +0100

    fixed errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
-  before_create :set_remember_token
+  # before_create :remember
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -23,7 +23,7 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  def set_remember_token 
+  def remember 
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
