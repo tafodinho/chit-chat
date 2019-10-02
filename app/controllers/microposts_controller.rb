e4340dc28d911187abcbc9615359087a96dd0646
Tafang Joshua <tafodinho@gmail.com>
2019-10-02 02:03:00 +0100

    added linting

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,22 +1,23 @@
 class MicropostsController < ApplicationController
-    before_action :require_login, only: [:new, :create]
+  before_action :require_login, only: [:new, :create]
 
-    def index
-      @microposts = Micropost.all
-    end
+  def index
+    @microposts = Micropost.all
+  end
 
-    def new
-      @micropost = Micropost.new
-    end
+  def new
+    @micropost = Micropost.new
+  end
 
-    def create
-      @micropost = @current_user.microposts.create(micropost_params)
-      flash[:success] = "New post created"
-      redirect_to action: "index"
-    end
-     
-    private
-      def micropost_params
-        params.require(:micropost).permit(:content)
-      end
+  def create
+    @micropost = @current_user.microposts.create(micropost_params)
+    flash[:success] = 'New post created'
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
 end
