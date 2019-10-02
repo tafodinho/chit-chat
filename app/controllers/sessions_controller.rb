1c9db28a0f3ef8b58e84abd4a0802b25afd59a16
Tafang Joshua <tafodinho@gmail.com>
2019-10-02 02:52:24 +0100

    linting fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,8 +27,8 @@ module SessionsHelper
 
   def remember(user)
     user.remember
-    cookies.permanent.signed[:user_id] = user.id 
-    cookies.permanent[:remember_token] = user.remember_token 
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def forget(user)
